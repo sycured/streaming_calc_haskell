f332cb29fe43d94d5bd77653fe115eb3002f9d6d
sycured <60801403+sycured@users.noreply.github.com>
2020-02-25 10:19:07 -0500

    replace let result by where

--- src/Lib.hs
+++ src/Lib.hs
@@ -14,29 +14,28 @@ serverUsageBw nblisteners bitrate nbdays nbhours =
   nbdays * nbhours * 3600 * bitrate * 1000 / 8 * nblisteners / 1024 / 1024
 
 printBwServer :: Float -> Float -> IO ()
-printBwServer nblisteners bitrate = do
-  let result = bwServer nblisteners bitrate
-  putStrLn
-    (  "Number of listeners: "
-    ++ show nblisteners
-    ++ "\nBitrate (kb/s): "
-    ++ show bitrate
-    ++ "\nServer bandwidth (Mib/s): "
-    ++ show result
-    )
+printBwServer nblisteners bitrate = putStrLn
+  (  "Number of listeners: "
+  ++ show nblisteners
+  ++ "\nBitrate (kb/s): "
+  ++ show bitrate
+  ++ "\nServer bandwidth (Mib/s): "
+  ++ show result
+  )
+  where result = bwServer nblisteners bitrate
+
 
 printServerUsageBw :: Float -> Float -> Float -> Float -> IO ()
-printServerUsageBw nblisteners bitrate nbdays nbhours = do
-  let result = serverUsageBw nblisteners bitrate nbdays nbhours
-  putStrLn
-    (  "Number of listeners: "
-    ++ show nblisteners
-    ++ "\nBitrate (kb/s): "
-    ++ show bitrate
-    ++ "\nNumber of days: "
-    ++ show nbdays
-    ++ "\nNumber of hours by days: "
-    ++ show nbhours
-    ++ "\nBandwidth used (GiB): "
-    ++ show result
-    )
+printServerUsageBw nblisteners bitrate nbdays nbhours = putStrLn
+  (  "Number of listeners: "
+  ++ show nblisteners
+  ++ "\nBitrate (kb/s): "
+  ++ show bitrate
+  ++ "\nNumber of days: "
+  ++ show nbdays
+  ++ "\nNumber of hours by days: "
+  ++ show nbhours
+  ++ "\nBandwidth used (GiB): "
+  ++ show result
+  )
+  where result = serverUsageBw nblisteners bitrate nbdays nbhours
